81641d50f821b9aab5b8329c6452aedc0c79d59b
Sanjero20 <gelosantos72@gmail.com>
2024-07-24 19:14:27 +0800

    feat: initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 function App() {
-  return <div className="h-screen bg-neutral-100">Giga Chat</div>;
+  return (
+    <div className="h-screen bg-neutral-100">
+      <main className="mx-auto flex h-full max-w-lg flex-col gap-2 bg-sky-400 p-4">
+        <header className="flex justify-between">
+          <h1 className="text-xl font-bold uppercase">Giga Chat</h1>
+
+          <input value={"@username"} className="w-[15ch]" />
+        </header>
+
+        {/* chat container */}
+        <div className="w-full flex-1 rounded bg-white">
+          {/*  */}
+          {/*  */}
+        </div>
+
+        {/* Message box */}
+        <fieldset className="flex gap-2">
+          <textarea className="w-full resize-none rounded" />
+          <button className="w-32 rounded bg-blue-700">Send</button>
+        </fieldset>
+      </main>
+    </div>
+  );
 }
 
 export default App;
