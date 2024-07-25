b912c350601b1c1d2a7a484e0794a5a09f8f0858
Sanjero20 <gelosantos72@gmail.com>
2024-07-25 19:06:25 +0800

    feat: use shadcn components for message field

--- src/components/MessageField.tsx
+++ src/components/MessageField.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
 const MessageField = () => {
   return (
     <fieldset className="flex gap-2">
-      <textarea className="w-full resize-none rounded p-1 shadow" />
-      <button className="w-32 rounded bg-gray-900 text-white shadow">
-        Send
-      </button>
+      <Textarea
+        className="min-h-10 w-full resize-none shadow"
+        placeholder="Send a message..."
+      />
+      <Button className="w-32 shadow">Send</Button>
     </fieldset>
   );
 };
